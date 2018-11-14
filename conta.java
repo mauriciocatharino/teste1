0ed792a4f3d5b4a3c6dc60f7cf01dcda68445002
Mauricio Prado Catharino <mauriciocatharino@gmail.com>
2018-11-14 17:44:34 -0300

    Testando envio de projeto do eclipse

---
+++ conta.java
@@ -0,0 +1,14 @@
+package teste1;
+
+public class conta {
+
+	double saldo = 100;
+	double limite = 100;
+
+	public void saldoLimite() {
+
+		System.out.println("O saldo mais o limite é --> " + (this.saldo + this.limite));
+
+	}
+
+}
---
+++ principal.java
@@ -0,0 +1,12 @@
+package teste1;
+
+public class principal {
+	public static void main (String[] args) {
+		
+		conta c = new conta();
+		
+		c.saldoLimite();
+		
+	}
+
+}
